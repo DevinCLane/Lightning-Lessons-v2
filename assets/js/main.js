d7b76bf0462e6ed186ea22180c8363d606697b01
Devin Lane <devin.c.lane@gmail.com>
2020-07-11 11:26:55 -0700

    copy and icon updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,7 +81,6 @@
 			});
 
 		}
-
 	// Gallery.
 		$('.gallery')
 			.on('click', 'a', function(event) {
@@ -206,3 +205,19 @@
 					});
 
 })(jQuery);
+
+// 	// Accordion
+// 	​var acc = document.getElementsByClassName("accordion");
+// 	var i;
+
+// 	for (i = 0; i < acc.length; i++) {
+//   		acc[i].addEventListener("click", function() {
+//     	this.classList.toggle("active");
+//     	var panel = this.nextElementSibling;
+//     	if (panel.style.display === "block") {
+//       		panel.style.display = "none";
+//     	} else {
+//       		panel.style.display = "block";
+//     	}
+//   		});
+// }	
